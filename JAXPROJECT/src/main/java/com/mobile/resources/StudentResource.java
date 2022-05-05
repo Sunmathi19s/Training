3e7e0b5b1efb8ba9667f6b37063b0a3d79ca0914
Sunmathi.S <Sunmathi.S@EXIND3010-99.allfon.in>
2022-05-05 15:35:30 +0530

    second postman commit

--- JAXPROJECT/src/main/java/com/mobile/resources/StudentResource.java
+++ JAXPROJECT/src/main/java/com/mobile/resources/StudentResource.java
@@ -1,10 +1,15 @@
 package com.mobile.resources;
 
 import java.util.List;
+import java.util.Map;
+
+import org.glassfish.grizzly.http.util.HttpStatus;
 
 import com.mobile.Student;
 import com.mobile.services.StudentService;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
@@ -14,6 +19,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/students")
 public class StudentResource {
@@ -36,31 +42,51 @@ public class StudentResource {
 	@GET
 	@Path("/{rollNumber}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Student findById(@PathParam("rollNumber")int id) {
+	public Response findById(@PathParam("rollNumber")int id) {
 	 
-	return service.findById(id).orElseThrow(() -> new RuntimeException("Id Not Found"));
+	 
+	 try {
+		 Student entity = service.findById(id)
+				 .orElseThrow(() -> new RuntimeException("Id Not Found"));
+				 return Response.ok(entity).build();
+	 }catch (RuntimeException e) {
+		 e.printStackTrace();
+		 
+		 JsonObject obj = Json.createObjectBuilder().add("id not found", "").build();
+		 
+		 return Response.ok("id not found").status(400).build();
+	 }
+	
 	}
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Student add(Student student) {
+	public Response add(Student student) {
 		
 	 
 	boolean result = service.add(student);
 	
 	if(result) {
-		return student;
+		return Response.ok(student).status(201).build();
 	}else {
-		return null;
+		return Response.status(400,"Not Created").build();
 	}
 	}
 	
 	@DELETE
 	@Path("/{rollNumber}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public boolean  remove(@PathParam("rollNumber")int id) {
-		return service.remove(id);
+	public Response  remove(@PathParam("rollNumber")int id) {
+		boolean result = service.remove(id);
+		
+		if(result) {
+			return Response.ok("one record deleted").build();
+		}else {
+			return Response.status(400,"Not Deleted").build();
+		}
 	}
+	
+	
 	@PUT
 	@Path("/{rollNumber}")
 	@Produces(MediaType.APPLICATION_JSON)
