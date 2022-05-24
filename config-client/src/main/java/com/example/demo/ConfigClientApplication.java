3621f4bfa70d9246e45e05c2b1b0c01b3eec4148
Sunmathi.S <Sunmathi.S@EXIND3010-99.allfon.in>
2022-05-24 14:04:20 +0530

    config-client commit

---
+++ config-client/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repos.ProductRepository;
+
+import lombok.Value;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ConfigClientApplication {
+//	@Value("${customer.firstname}")
+//	private String name;
+//
+//
+//	@GetMapping(path="/names")
+//	public String getName()
+//	{
+//		return this.name;
+//		
+//	}
+	
+	@Autowired
+	private ProductRepository repo;	
+	@GetMapping(path = "/products")
+	public List<Product> getName() {
+		return this.repo.findAll();
+	}
+
+
+	public static void main(String[] args) {
+	ConfigurableApplicationContext	ctx = SpringApplication.run(ConfigClientApplication.class, args);
+	
+//	ProductRepository repo = ctx.getBean(ProductRepository.class);
+//	repo.findAll().forEach(System.out::println);
+	}
+
+}
