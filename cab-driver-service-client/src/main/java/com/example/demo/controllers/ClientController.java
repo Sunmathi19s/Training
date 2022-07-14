cd13d8b84ee7ab8869c079513488887945f3f211
Sunmathi.S <Sunmathi.S@EXIND3010-99.allfon.in>
2022-07-14 14:54:33 +0530

    14-7 commit

--- cab-driver-service-client/src/main/java/com/example/demo/controllers/ClientController.java
+++ cab-driver-service-client/src/main/java/com/example/demo/controllers/ClientController.java
@@ -1,5 +1,11 @@
 package com.example.demo.controllers;
 
+import static java.util.stream.Collectors.toSet;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.CabDriver;
+import com.example.demo.TripDetail;
+import com.example.demo.dto.TripDto;
+
 
 @RestController
 @RequestMapping(path = "/client")
@@ -15,6 +24,25 @@ public class ClientController {
 	
 	@Autowired
 	private RestTemplate template;
+	
+	@Autowired
+	private TripDto dto;
+	
+	
+	@GetMapping(path = "/tripdetails")
+	public String gettrips() {
+		return this.template.getForObject("http://TRIP-DETAIL-SERVICE/api/v1/tripdetails",
+				String.class);
+	}
+	
+	
+	@GetMapping(path = "/srch/tripdetails/{id}")
+	public String getDriverById(@PathVariable("id") int id) {
+		return this.template.getForObject("http://TRIP-DETAIL-SERVICE/api/v1/tripdetails/srch/"+id,
+				String.class);
+	}
+	
+	
 	
 	@GetMapping(path = "/drivers")
 	public String getDrivers() {
@@ -34,6 +62,31 @@ public class ClientController {
 	public String getDriverByName(@PathVariable("name") String name){
 		return this.template.getForObject("http://CAB-DRIVER-SERVICE/api/v1/drivers/srch/"+name, String.class);
 		
+	}
+	
+
+	@GetMapping(path="/driver/trips/{id}")
+	public TripDto getDriverTrips(@PathVariable("id")  int id){
+		CabDriver driver =  this.template.getForObject("http://CAB-DRIVER-SERVICE/api/v1/drivers/"+id, 
+				CabDriver.class);
+		TripDetail[] trips = this.template.getForObject("http://TRIP-DETAIL-SERVICE/api/v1/tripdetails/srch/"+id,
+				TripDetail[].class);
+		
+		dto.setDriver(driver);
+		
+		//-------------in list---------------
+		//List<TripDetail> detailSet = Arrays.asList(trips);
+		//dto.setTrips(detailSet);
+		
+		
+
+		//---------------------in set-------------
+		Set<TripDetail> detailSet=Arrays.stream(trips).collect(toSet());
+			
+		dto.setTrips(detailSet);
+				
+				
+		return dto;
 	}
 
 }
---
+++ cab-driver-service-client/src/main/java/com/example/demo/dto/TripDto.java
@@ -0,0 +1,31 @@
+package com.example.demo.dto;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.CabDriver;
+import com.example.demo.TripDetail;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+
+@Component
+public class TripDto {
+	
+	
+	private  CabDriver driver;
+	//private List<TripDetail> trips;
+	private Set<TripDetail> trips;
+
+}
